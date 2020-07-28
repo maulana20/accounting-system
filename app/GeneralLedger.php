03a23e565fe760660a505157a9c69ca232e4d923
maulana <maulana.saputra@aqi.co.id>
2020-07-28 23:00:15 +0700

    revisi general ledger
    Signed-off-by: maulana <maulana.saputra@aqi.co.id>

---
+++ app/GeneralLedger.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\FinancialTrans;
+
+class GeneralLedger extends Model
+{
+    public function record($coa_id)
+    {
+        $result = FinancialTrans::selectRaw('gl_analysis.coa_to, gl_analysis.coa_from, coa.code, coa.name, financial_trans.id, financial_trans.period_begin, financial_trans.created_at, gl_analysis.desc')
+            ->selectRaw('(SELECT balance FROM posting WHERE posting.coa_id=1 AND posting.period_begin=201810) as begining')
+            ->selectRaw('(CASE WHEN gl_analysis.position = 2 THEN gl_analysis.value ELSE 0 END) AS debet, (CASE WHEN gl_analysis.position = 1 THEN gl_analysis.value ELSE 0 END) AS credit')
+            ->selectRaw('(SELECT balance FROM posting WHERE posting.coa_id=1 AND posting.period_begin=201810) + (SELECT @ending := @ending+(CASE WHEN gl_analysis.position = 2 THEN gl_analysis.value ELSE gl_analysis.value * -1 END) FROM (SELECT @ending := 0) i) As ending')
+            ->join('gl_analysis', 'gl_analysis.financial_trans_id', '=', 'financial_trans.id')
+            ->join('period', 'period.begin', '=', 'financial_trans.period_begin')
+            ->join('coa', 'coa.id', '=', 'gl_analysis.coa_from')
+            ->where('gl_analysis.coa_to', $coa_id)
+            ->where('financial_trans.period_begin', '201811')
+            ->orderBy('gl_analysis.coa_to', 'ASC')
+            ->get();
+        
+        return $result;
+    }
+    
+    public function search($coa_id, $from_date, $to_date)
+    {
+        $financial_trans = FinancialTrans::selectRaw('gl_analysis.coa_to as coa_id, coa.code as coa_to')
+            ->join('gl_analysis', 'gl_analysis.financial_trans_id', '=', 'financial_trans.id')
+            ->join('coa', 'coa.id', '=', 'gl_analysis.coa_to')
+            ->where('financial_trans.period_begin', '201811')
+            ->groupBy('gl_analysis.coa_to', 'coa.code')
+            ->orderBy('financial_trans.id', 'ASC')
+            ->get();
+        
+        $result = [];
+        foreach ($financial_trans as $value) {
+            foreach ($this->record($value->coa_id) as $data) {
+                $data['coa_to'] = $value->coa_to;
+                $result[] = $data;
+            }
+        }
+        
+        return $result;
+    }
+}
--- app/Http/Controllers/Accounting/GeneralLegderController.php
+++ app/Http/Controllers/Accounting/GeneralLegderController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
 
-use App\FinancialTrans;
+use App\GeneralLedger;
 
 class GeneralLegderController extends Controller
 {
@@ -16,17 +16,7 @@ class GeneralLegderController extends Controller
         $to_date = $request->get('to_date');
         $coa_id = $request->get('coa_id');
         
-        $general_ledger = FinancialTrans::selectRaw('gl_analysis.coa_to, gl_analysis.coa_from, coa.code, coa.name, financial_trans.id, financial_trans.period_begin, financial_trans.created_at, gl_analysis.desc')
-            ->selectRaw('(SELECT balance FROM posting WHERE posting.coa_id=1 AND posting.period_begin=201810) as begining')
-            ->selectRaw('(CASE WHEN gl_analysis.position = 2 THEN gl_analysis.value ELSE 0 END) AS debet, (CASE WHEN gl_analysis.position = 1 THEN gl_analysis.value ELSE 0 END) AS credit')
-            ->selectRaw('(SELECT balance FROM posting WHERE posting.coa_id=1 AND posting.period_begin=201810) + (SELECT @ending := @ending+(CASE WHEN gl_analysis.position = 2 THEN gl_analysis.value ELSE gl_analysis.value * -1 END) FROM (SELECT @ending := 0) i) As ending')
-            ->join('gl_analysis', 'gl_analysis.financial_trans_id', '=', 'financial_trans.id')
-            ->join('period', 'period.begin', '=', 'financial_trans.period_begin')
-            ->join('coa', 'coa.id', '=', 'gl_analysis.coa_from')
-            ->where('gl_analysis.coa_to', 1)
-            ->where('financial_trans.period_begin', '201811')
-            ->orderBy('gl_analysis.coa_to', 'ASC')
-            ->get();
+        $general_ledger = (new GeneralLedger())->search($coa_id, $from_date, $to_date);
         
         return view('accounting.general-ledger.index', compact('general_ledger', 'from_date', 'to_date', 'coa_id'));
     }
